694f3dc96b19844431a78914fa96a80dec5ffcef
johnkirtley <kirtleyj16@gmail.com>
2020-02-18 18:18:38 -0800

    Total amount reflects appropfiate dollar amount. Also, you cannot add an item twice.

--- src/reducers/carListReducer.js
+++ src/reducers/carListReducer.js
@@ -19,25 +19,39 @@ export const initialState = {
 
 
 export const carListReducer = (state = initialState, action) => {
+
+    const itemTracker = state.car.features.map(item => {
+        return item.id
+    })
+    console.log(itemTracker)
+
     switch (action.type) {
         case 'ADD_FEATURE':
             // console.log('add feature', state, action)
-            return {
-                ...state,
-                car: { ...state.car, price: state.car.price + action.payload.price, features: [...state.car.features, action.payload] }
-            };
+            if (itemTracker.includes(action.payload.id)) {
+                alert('Already added')
+                return { ...state }
+            } else {
+                return {
+                    ...state,
+                    car: { ...state.car, price: state.car.price + action.payload.price, features: [...state.car.features, action.payload] }
+                };
+            }
         case 'REMOVE_FEATURE':
-            console.log('remove feature', state, action)
-            let index = state.car.features.indexOf(action.payload) + 1
-            console.log(index)
-            let remove = state.car.features.splice(index, 1)
-            console.log(remove)
+            // console.log('remove feature', state, action)
+            let itemIndex = state.car.features.indexOf(action.payload) + 1
+            // console.log(index)
+            let remove = state.car.features.splice(itemIndex, 1)
+            // console.log(remove)
+            let subtract = remove.map(i => i.price)
+            console.log(subtract)
+            // console.log(remove)
             // let filter = state.car.features.filter(feature => {
             //     return feature.id !== action.payload.id;
             // })
             return {
                 ...state,
-                car: { ...state.car, features: [...state.car.features] }
+                car: { ...state.car, price: state.car.price - Number(subtract), features: [...state.car.features] }
             }
         default:
             return state;
